140eb5b403b4ef104f59dee58b988d663c866220
Adelani <harounadelani@gmail.com>
2022-10-02 00:37:30 +0100

    added typewriter animation

--- src/home.jsx
+++ src/home.jsx
@@ -6,7 +6,13 @@ export default function Home () {
     return (
         <main id="home" className=" container mx-auto md:px-44 flex flex-col justify-center h-screen space-y-2 md:space-y-4 text-[#45505b]">
             <h1 className="font-bold md:text-6xl text-3xl  self-center md:self-start tracking-wider">Bello Adelani</h1>
-            <p className=" self-center text-2xl md:self-start text-teal-600 md:tracking-widest">Software Developer</p>
+            <p className="self-center md:text-2xl text-xl md:self-start text-teal-600 md:tracking-widest">
+                <ul className="dynamic-txts">
+                    <li><span>Software Developer</span></li>
+                    <li><span>FrontEnd Engineer</span></li>
+                    <li><span>Tech Enthusiast</span></li>
+                </ul> 
+            </p>
             <div className="space-x-6 self-center md:self-start flex items-center pt-3 text-2xl hover:text-teal-900">
                 <a href="https://github.com/Adelani10">
                     <i className="fa-brands fa-github"></i>
