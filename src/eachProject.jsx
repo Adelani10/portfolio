b7926949183f888728a1f6befee9516ac12ea408
Adelani <harounadelani@gmail.com>
2022-10-07 00:07:38 +0100

    cleaning up

--- src/eachProject.jsx
+++ src/eachProject.jsx
@@ -7,9 +7,12 @@ export default function Each ({id, live, image, name, summary, tools, githubRepo
 
 
     return (
-        <div key={id} className=" h-auto bg-stone-100 hover:shadow-lg shadow-black rounded-sm">
+        <div key={id} className=" h-auto bg-stone-100 hover:shadow-lg shadow-black rounded-lg">
                 <a href={live} target='_blank' className="mx-auto flex flex-col items-center hover:shadow-lg shadow-black">
-                    <img src={image} alt="image" className="w-full h-72 rounded-t-sm" />
+                    <div className="w-full h-72 rounded-t-lg">
+                        <img src={image} alt="image" className="w-full h-full rounded-t-lg md:object-cover" />
+                    </div>
+
                     <section className="font-bold flex items-center space-x-2 text-sky-400 py-2">
                         <h1 className=" text-sm ">visit</h1>
                         <i className="fa-solid fa-up-right-from-square"></i>
@@ -21,7 +24,7 @@ export default function Each ({id, live, image, name, summary, tools, githubRepo
                     <h1 className="font-bold md:text-2xl text-xl capitalize">
                         {name}
                     </h1>
-                    <p className="inline-block font-pop">
+                    <p className="inline-block font-pop md:hidden lg:block">
                         {seeMore ? summary : `${summary.substring(0, 65)}...`} <button onClick={()=> setSeeMore(!seeMore)} 
                         className=" text-sky-400">{seeMore ? 'see less' : 'see more'}</button>
                     </p>
